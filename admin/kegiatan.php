ad7c0797ffccb8c0497c691fc7fdde7811ca7583
zainpelope <palengaancity12345@gmail.com>
2025-02-03 10:15:24 -0800

    kegiatan finish

--- admin/kegiatan.php
+++ admin/kegiatan.php
@@ -1,20 +1,69 @@
-<main class="main">
-    <!-- Hero Title -->
-    <div class="page-title dark-background">
-        <!-- <div class="container position-relative">
-            <h1>Visi - Misi</h1>
-            <p>Esse dolorum voluptatum ullam est sint nemo et est ipsa porro placeat quibusdam quia assumenda numquam molestias.</p>
-        </div> -->
-    </div>
+<?php
+include 'koneksi.php';
 
+$sql = "SELECT * FROM kegiatan";
+$result = $conn->query($sql);
+?>
+
+<main class="main">
+    <div class="page-title dark-background"></div>
     <div class="container my-5" data-aos="fade-up" data-aos-delay="200">
-        <!-- Profil Section -->
-       kegiatan
+        <section id="visimisi" class="vision-mission mt-5">
+            <h1>Kegiatan</h1>
+
+            <a href="tambah.php" class="btn btn-success mb-3">Tambah Kegiatan</a>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Gambar</th>
+                        <th scope="col">Nama Kegiatan</th>
+                        <th scope="col">Keterangan</th>
+                        <th scope="col">Tanggal Mulai</th>
+                        <th scope="col">Tanggal Selesai</th>
+                        <th scope="col">Lokasi</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($result->num_rows > 0) {
+                        $no = 1;
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $no . "</td>";
+                            echo "<td><img src='admin/uploads/" . $row['gambar'] . "' width='100'></td>";
+                            echo "<td>" . $row['nama_kegiatan'] . "</td>";
+                            echo "<td>" . $row['keterangan'] . "</td>";
+
+                            $tanggal_mulai = date("d-m-Y", strtotime($row['tanggal_mulai']));
+                            echo "<td>" . $tanggal_mulai . "</td>";
+
+                     
+                            $tanggal_selesai = date("d-m-Y", strtotime($row['tanggal_selesai']));
+                            echo "<td>" . $tanggal_selesai . "</td>";
+
+                            echo "<td>" . $row['lokasi'] . "</td>";
+
+                            echo "<td>
+                                <a href='detail_kegiatan.php?id=" . $row['id_kegiatan'] . "' class='btn btn-info btn-sm'>Detail</a>
+                                <a href='edit_kegiatan.php?id=" . $row['id_kegiatan'] . "' class='btn btn-warning btn-sm'>Edit</a>
+                                <a href='hapus.php?id=" . $row['id_kegiatan'] . "' class='btn btn-danger btn-sm' 
+                                    onclick='return confirm(\"Apakah Anda yakin ingin menghapus kegiatan ini?\");'>Hapus</a>
+                            </td>";
+                            echo "</tr>";
+                            $no++;
+                        }
+                    } else {
+                        echo "<tr><td colspan='8'>Tidak ada data kegiatan.</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </section>
     </div>
 </main>
-<?php include('footer.html'); ?>
-<!-- Scroll Top -->
-<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<!-- Preloader -->
+<?php include('footer.html'); ?>
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 <div id="preloader"></div>
--- pages/kegiatan.php
+++ pages/kegiatan.php
@@ -1,35 +1,67 @@
-<main class="main">
-    <!-- Hero Title -->
-    <div class="page-title dark-background">
-        <!-- <div class="container position-relative">
-            <h1>Visi - Misi</h1>
-            <p>Esse dolorum voluptatum ullam est sint nemo et est ipsa porro placeat quibusdam quia assumenda numquam molestias.</p>
-        </div> -->
-    </div>
+<?php
+include 'koneksi.php';
 
+$sql = "SELECT * FROM kegiatan";
+$result = $conn->query($sql);
+?>
+
+<main class="main">
+    <div class="page-title dark-background"></div>
     <div class="container my-5" data-aos="fade-up" data-aos-delay="200">
-        <!-- Profil Section -->
         <section id="visimisi" class="vision-mission mt-5">
-            <div class="row">
-                <div class="col-md-6">
-                    <h3>VISI</h3>
-                    <p>Terciptanya tenaga kerja yang terampil, ahli, produktif dan kompeten melalui pelatihan kerja yang mendukung perluasan kesempatan kerja dan lapangan kerja, peningkatan produktifitas, memenuhi kebutuhan masyarakat, mendorong pertumbuhan usaha kecil dan menengah dan perekonomian daerah/nasional.</p>
-                </div>
-                <div class="col-md-6">
-                    <h3>MISI</h3>
-                    <ul>
-                        <li>Meningkatkan kualitas dan kompetensi tenaga kerja melalui berbagai jenis pelatihan keterampilan dan keahlian...</li>
-                        <li>Meningkatkan kualitas dan kuantitas tenaga kerja melalui pelatihan kerja dalam rangka mengurangi pengangguran...</li>
-                        <li>Menyelenggarakan pelatihan yang berbasis kompetensi...</li>
-                    </ul>
-                </div>
-            </div>
+            <h1>Kegiatan</h1>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Gambar</th>
+                        <th scope="col">Nama Kegiatan</th>
+                        <th scope="col">Keterangan</th>
+                        <th scope="col">Tanggal Mulai</th>
+                        <th scope="col">Tanggal Selesai</th>
+                        <th scope="col">Lokasi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($result->num_rows > 0) {
+                        $no = 1;
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $no . "</td>";
+                            echo "<td><img src='admin/uploads/" . $row['gambar'] . "' width='100'></td>";
+                            echo "<td>" . $row['nama_kegiatan'] . "</td>";
+                            
+                            $keterangan = $row['keterangan'];
+                            $keterangan_pendek = substr($keterangan, 0, 150); // Batasi 150 karakter
+                            echo "<td>" . nl2br($keterangan_pendek);
+                            if (strlen($keterangan) > 150) {
+                                echo "... <a href='kegiatan_detail.php?id=" . $row['id_kegiatan'] . "' style='color: blue; font-style: italic;'>Lihat Selengkapnya</a>";
+
+                            }
+                            echo "</td>";
+                            
+                            $tanggal_mulai = date("d-m-Y", strtotime($row['tanggal_mulai']));
+                            echo "<td>" . $tanggal_mulai . "</td>";
+                            
+                            $tanggal_selesai = date("d-m-Y", strtotime($row['tanggal_selesai']));
+                            echo "<td>" . $tanggal_selesai . "</td>";
+                            
+                            echo "<td>" . $row['lokasi'] . "</td>";
+                            echo "</tr>";
+                            
+                            $no++;
+                        }
+                    } else {
+                        echo "<tr><td colspan='8'>Tidak ada data kegiatan.</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
         </section>
     </div>
 </main>
-<?php include('footer.html'); ?>
-<!-- Scroll Top -->
-<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<!-- Preloader -->
+<?php include('footer.html'); ?>
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 <div id="preloader"></div>
