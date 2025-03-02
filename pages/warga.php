5d17cc172d541c7d6b3a03a0bd0d877b9e22dcbd
alapolaa <zain.alapolaa@gmail.com>
2025-03-02 14:56:29 -0800

    menambahkan form pencarian

--- pages/warga.php
+++ pages/warga.php
@@ -13,19 +13,26 @@ $query_user = "SELECT * FROM pengguna WHERE id = '$id_pengguna'";
 $result_user = $conn->query($query_user);
 $user = $result_user->fetch_assoc();
 
-// Pagination
-$batas = 2; // Jumlah data per halaman
+
+$batas = 5;
 $halaman = isset($_GET['halaman']) ? (int)$_GET['halaman'] : 1;
 $halaman_awal = ($halaman > 1) ? ($halaman * $batas) - $batas : 0;
 
 $previous = $halaman - 1;
 $next = $halaman + 1;
 
-$data = mysqli_query($conn, "SELECT * FROM pengajuan_surat WHERE id_pengguna = '$id_pengguna'");
+
+$cari = isset($_GET['cari']) ? $_GET['cari'] : '';
+$where = "";
+if ($cari != '') {
+    $where = "AND jenis_surat LIKE '%$cari%'";
+}
+
+$data = mysqli_query($conn, "SELECT * FROM pengajuan_surat WHERE id_pengguna = '$id_pengguna' $where");
 $jumlah_data = mysqli_num_rows($data);
 $total_halaman = ceil($jumlah_data / $batas);
 
-$query_surat = "SELECT * FROM pengajuan_surat WHERE id_pengguna = '$id_pengguna' ORDER BY tanggal_pengajuan DESC LIMIT $halaman_awal, $batas";
+$query_surat = "SELECT * FROM pengajuan_surat WHERE id_pengguna = '$id_pengguna' $where ORDER BY tanggal_pengajuan DESC LIMIT $halaman_awal, $batas";
 $result_surat = $conn->query($query_surat);
 ?>
 
@@ -38,6 +45,15 @@ $result_surat = $conn->query($query_surat);
         <hr>
 
         <h3>Status Pengajuan Surat</h3>
+
+        <form method="GET" action="">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Cari Jenis Surat" name="cari" value="<?= $cari; ?>">
+                <input type="hidden" name="page" value="warga">
+                <button class="btn btn-outline-secondary" type="submit">Cari</button>
+            </div>
+        </form>
+
         <table class="table">
             <thead>
                 <tr>
@@ -73,20 +89,22 @@ $result_surat = $conn->query($query_surat);
         <nav aria-label="Page navigation example">
             <ul class="pagination">
                 <?php if ($halaman > 1) { ?>
-                    <li class="page-item"><a class="page-link" href="?page=warga&halaman=<?= $previous; ?>">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="?page=warga&cari=<?= $cari; ?>&halaman=<?= $previous; ?>">Previous</a></li>
                 <?php } ?>
                 <?php
                 for ($x = 1; $x <= $total_halaman; $x++) {
                 ?>
-                    <li class="page-item <?= ($halaman == $x) ? 'active' : ''; ?>"><a class="page-link" href="?page=warga&halaman=<?= $x; ?>"><?= $x; ?></a></li>
+                    <li class="page-item <?= ($halaman == $x) ? 'active' : ''; ?>"><a class="page-link" href="?page=warga&cari=<?= $cari; ?>&halaman=<?= $x; ?>"><?= $x; ?></a></li>
                 <?php
                 }
                 ?>
                 <?php if ($halaman < $total_halaman) { ?>
-                    <li class="page-item"><a class="page-link" href="?page=warga&halaman=<?= $next; ?>">Next</a></li>
+                    <li class="page-item"><a class="page-link" href="?page=warga&cari=<?= $cari; ?>&halaman=<?= $next; ?>">Next</a></li>
                 <?php } ?>
             </ul>
         </nav>
+
+    </div>
 
 </main>
 
